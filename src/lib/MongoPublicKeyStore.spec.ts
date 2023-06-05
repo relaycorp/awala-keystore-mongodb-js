6a95fa4d0648ee3320c4f0bb5493392d311e950b
Gus Narea <gnarea@users.noreply.github.com>
2023-06-05 18:03:10 +0100

    fix

--- src/lib/MongoPublicKeyStore.spec.ts
+++ src/lib/MongoPublicKeyStore.spec.ts
@@ -7,6 +7,8 @@ import {
   getIdFromIdentityKey,
   SessionKey,
 } from '@relaycorp/relaynet-core';
+import { getModelForClass } from '@typegoose/typegoose';
+import { addSeconds } from 'date-fns';
 
 import { PeerPublicKeyData } from './models';
 import { MongoPublicKeyStore } from './MongoPublicKeyStore';
@@ -31,6 +33,11 @@ beforeAll(async () => {
   peerPublicKeyData.creationDate = new Date();
 });
 
+let PEER_SESSION_KEY: SessionKey;
+beforeAll(() => {
+  PEER_SESSION_KEY = { keyId: peerPublicKeyData.keyId, publicKey: peerPublicKey };
+});
+
 test('Identity keys should not yet be supported', async () => {
   const store = new MongoPublicKeyStore(getConnection());
 
@@ -45,61 +52,44 @@ test('Identity keys should not yet be supported', async () => {
   );
 });
 
-describe('retrieveSessionKeyData', () => {
-  test('Key should be looked up by the private node address of the peer', async () => {
-    const store = new MongoPublicKeyStore(getConnection());
+describe('saveSessionKeyData', () => {
+  test('Key should be created if it does not exist', async () => {
+    const connection = getConnection();
+    const store = new MongoPublicKeyStore(connection);
 
-    await store.retrieveLastSessionKey(peerId);
+    await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
 
-    expect(mockFindOne).toBeCalledTimes(1);
-    expect(mockFindOne).toBeCalledWith({ peerId });
+    const keyDataModel = getModelForClass(PeerPublicKeyData, { existingConnection: connection });
+    await expect(keyDataModel.exists(peerPublicKeyData)).resolves.toBeTruthy();
   });
 
+  test('Key should be updated if it exists', async () => {
+    const connection = getConnection();
+    const store = new MongoPublicKeyStore(connection);
+    await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
+    const creationDate = addSeconds(peerPublicKeyData.creationDate, 1);
+
+    await store.saveSessionKey(PEER_SESSION_KEY, peerId, creationDate);
+
+    const keyDataModel = getModelForClass(PeerPublicKeyData, { existingConnection: connection });
+    await expect(keyDataModel.exists({ ...peerPublicKeyData, creationDate })).resolves.toBeTruthy();
+  });
+});
+
+describe('retrieveSessionKeyData', () => {
   test('Existing key should be returned', async () => {
     const store = new MongoPublicKeyStore(getConnection());
+    await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
 
     const key = await store.retrieveLastSessionKey(peerId);
 
-    expect(key?.keyId).toEqual(peerPublicKeyData.keyId);
+    expect(key!.keyId.equals(peerPublicKeyData.keyId)).toBeTrue();
     expect(await derSerializePublicKey(key!.publicKey)).toEqual(peerPublicKeyData.keyDer);
   });
 
   test('Non-existing key should result in null', async () => {
     const store = new MongoPublicKeyStore(getConnection());
-    mockFindOneExec.mockResolvedValue(null);
 
     await expect(store.retrieveLastSessionKey(peerId)).resolves.toBeNull();
-  });
-});
-
-describe('saveSessionKeyData', () => {
-  let PEER_SESSION_KEY: SessionKey;
-  beforeAll(() => {
-    PEER_SESSION_KEY = { keyId: peerPublicKeyData.keyId, publicKey: peerPublicKey };
-  });
-
-  test('Existing connection should be used', async () => {
-    const store = new MongoPublicKeyStore(getConnection());
-
-    await store.saveSessionKey(PEER_SESSION_KEY, peerId, new Date());
-
-    expect(stubGetModelForClass).toBeCalledTimes(1);
-    expect(stubGetModelForClass).toBeCalledWith(PeerPublicKeyData, {
-      existingConnection: connection,
-    });
-  });
-
-  test('Key should be upserted', async () => {
-    const store = new MongoPublicKeyStore(getConnection());
-    const creationDate = new Date();
-
-    await store.saveSessionKey(PEER_SESSION_KEY, peerId, creationDate);
-
-    expect(mockUpdateOne).toBeCalledTimes(1);
-    expect(mockUpdateOne).toBeCalledWith(
-      { peerId },
-      { ...peerPublicKeyData, creationDate },
-      { upsert: true },
-    );
   });
 });
