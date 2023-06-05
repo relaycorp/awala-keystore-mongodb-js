a769c7d0e6cbb57754bd825d17cb6b96fc8076b4
Gus Narea <gnarea@users.noreply.github.com>
2023-06-05 21:42:18 +0100

    fix: Export classes

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { CertificationPath, PeerPublicKeyData } from './lib/models';
+export { MongoCertificateStore } from './lib/MongoCertificateStore';
+export { MongoPublicKeyStore } from './lib/MongoPublicKeyStore';
