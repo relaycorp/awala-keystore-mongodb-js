867ea28a4050e5a39bf8f761a723255ff937d82d
Gus Narea <gnarea@users.noreply.github.com>
2023-06-08 15:54:42 +0100

    feat: Add support for identity keys (#3)

--- src/lib/MongoPublicKeyStore.spec.ts
+++ src/lib/MongoPublicKeyStore.spec.ts
@@ -10,86 +10,131 @@ import {
 import { getModelForClass } from '@typegoose/typegoose';
 import { addSeconds } from 'date-fns';
 
-import { PeerPublicKeyData } from './models';
+import { IdentityPublicKeyData, PeerPublicKeyData } from './models';
 import { MongoPublicKeyStore } from './MongoPublicKeyStore';
 import { setUpTestDBConnection } from '../testUtils/db';
 
 const getConnection = setUpTestDBConnection();
 
-let peerPublicKeyData: PeerPublicKeyData;
-let peerPublicKey: CryptoKey;
 let peerId: string;
+let peerIdKeyPair: CryptoKeyPair;
 beforeAll(async () => {
-  const keyPair = await generateRSAKeyPair();
-  peerId = await getIdFromIdentityKey(keyPair.publicKey);
-
-  const sessionKeyPair = await generateECDHKeyPair();
-  peerPublicKey = sessionKeyPair.publicKey;
-
-  peerPublicKeyData = new PeerPublicKeyData();
-  peerPublicKeyData.peerId = peerId;
-  peerPublicKeyData.keyId = Buffer.from([1, 3, 5, 7]);
-  peerPublicKeyData.keyDer = await derSerializePublicKey(sessionKeyPair.publicKey);
-  peerPublicKeyData.creationDate = new Date();
+  peerIdKeyPair = await generateRSAKeyPair();
+  peerId = await getIdFromIdentityKey(peerIdKeyPair.publicKey);
 });
 
-let PEER_SESSION_KEY: SessionKey;
-beforeAll(() => {
-  PEER_SESSION_KEY = { keyId: peerPublicKeyData.keyId, publicKey: peerPublicKey };
-});
+describe('Identity keys', () => {
+  describe('saveIdentityKeySerialized', () => {
+    test('Key should be created if it does not exist', async () => {
+      const connection = getConnection();
+      const store = new MongoPublicKeyStore(connection);
 
-test('Identity keys should not yet be supported', async () => {
-  const store = new MongoPublicKeyStore(getConnection());
+      await store.saveIdentityKey(peerIdKeyPair.publicKey);
 
-  const identityKeyPair = await generateRSAKeyPair();
-  await expect(store.saveIdentityKey(identityKeyPair.privateKey)).rejects.toThrowWithMessage(
-    Error,
-    'Method not yet implemented',
-  );
-  await expect(store.retrieveIdentityKey('0deadbeef')).rejects.toThrowWithMessage(
-    Error,
-    'Method not yet implemented',
-  );
-});
+      const model = getModelForClass(IdentityPublicKeyData, { existingConnection: connection });
+      await expect(
+        model.exists({ peerId, keyDer: await derSerializePublicKey(peerIdKeyPair.publicKey) }),
+      ).resolves.toBeTruthy();
+    });
 
-describe('saveSessionKeyData', () => {
-  test('Key should be created if it does not exist', async () => {
-    const connection = getConnection();
-    const store = new MongoPublicKeyStore(connection);
+    test('Nothing should change if key already exists', async () => {
+      const connection = getConnection();
+      const store = new MongoPublicKeyStore(connection);
+      await store.saveIdentityKey(peerIdKeyPair.publicKey);
 
-    await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
+      await store.saveIdentityKey(peerIdKeyPair.publicKey);
 
-    const keyDataModel = getModelForClass(PeerPublicKeyData, { existingConnection: connection });
-    await expect(keyDataModel.exists(peerPublicKeyData)).resolves.toBeTruthy();
+      const model = getModelForClass(IdentityPublicKeyData, { existingConnection: connection });
+      await expect(
+        model.exists({ peerId, keyDer: await derSerializePublicKey(peerIdKeyPair.publicKey) }),
+      ).resolves.toBeTruthy();
+    });
   });
 
-  test('Key should be updated if it exists', async () => {
-    const connection = getConnection();
-    const store = new MongoPublicKeyStore(connection);
-    await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
-    const creationDate = addSeconds(peerPublicKeyData.creationDate, 1);
+  describe('retrieveIdentityKeySerialized', () => {
+    test('Existing key should be returned', async () => {
+      const connection = getConnection();
+      const store = new MongoPublicKeyStore(connection);
+      await store.saveIdentityKey(peerIdKeyPair.publicKey);
 
-    await store.saveSessionKey(PEER_SESSION_KEY, peerId, creationDate);
+      const key = await store.retrieveIdentityKey(peerId);
 
-    const keyDataModel = getModelForClass(PeerPublicKeyData, { existingConnection: connection });
-    await expect(keyDataModel.exists({ ...peerPublicKeyData, creationDate })).resolves.toBeTruthy();
+      await expect(derSerializePublicKey(key!)).resolves.toMatchObject(
+        await derSerializePublicKey(peerIdKeyPair.publicKey),
+      );
+    });
+
+    test('Non-existing key should result in null', async () => {
+      const connection = getConnection();
+      const store = new MongoPublicKeyStore(connection);
+
+      const key = await store.retrieveIdentityKey(peerId);
+
+      expect(key).toBeNull();
+    });
   });
 });
 
-describe('retrieveSessionKeyData', () => {
-  test('Existing key should be returned', async () => {
-    const store = new MongoPublicKeyStore(getConnection());
-    await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
+describe('Session keys', () => {
+  let peerPublicKeyData: PeerPublicKeyData;
+  let peerPublicKey: CryptoKey;
+  beforeAll(async () => {
+    const sessionKeyPair = await generateECDHKeyPair();
+    peerPublicKey = sessionKeyPair.publicKey;
 
-    const key = await store.retrieveLastSessionKey(peerId);
-
-    expect(key!.keyId.equals(peerPublicKeyData.keyId)).toBeTrue();
-    expect(await derSerializePublicKey(key!.publicKey)).toEqual(peerPublicKeyData.keyDer);
+    peerPublicKeyData = new PeerPublicKeyData();
+    peerPublicKeyData.peerId = peerId;
+    peerPublicKeyData.keyId = Buffer.from([1, 3, 5, 7]);
+    peerPublicKeyData.keyDer = await derSerializePublicKey(sessionKeyPair.publicKey);
+    peerPublicKeyData.creationDate = new Date();
   });
 
-  test('Non-existing key should result in null', async () => {
-    const store = new MongoPublicKeyStore(getConnection());
+  let PEER_SESSION_KEY: SessionKey;
+  beforeAll(() => {
+    PEER_SESSION_KEY = { keyId: peerPublicKeyData.keyId, publicKey: peerPublicKey };
+  });
 
-    await expect(store.retrieveLastSessionKey(peerId)).resolves.toBeNull();
+  describe('saveSessionKeyData', () => {
+    test('Key should be created if it does not exist', async () => {
+      const connection = getConnection();
+      const store = new MongoPublicKeyStore(connection);
+
+      await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
+
+      const keyDataModel = getModelForClass(PeerPublicKeyData, { existingConnection: connection });
+      await expect(keyDataModel.exists(peerPublicKeyData)).resolves.toBeTruthy();
+    });
+
+    test('Key should be updated if it exists', async () => {
+      const connection = getConnection();
+      const store = new MongoPublicKeyStore(connection);
+      await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
+      const creationDate = addSeconds(peerPublicKeyData.creationDate, 1);
+
+      await store.saveSessionKey(PEER_SESSION_KEY, peerId, creationDate);
+
+      const keyDataModel = getModelForClass(PeerPublicKeyData, { existingConnection: connection });
+      await expect(
+        keyDataModel.exists({ ...peerPublicKeyData, creationDate }),
+      ).resolves.toBeTruthy();
+    });
+  });
+
+  describe('retrieveSessionKeyData', () => {
+    test('Existing key should be returned', async () => {
+      const store = new MongoPublicKeyStore(getConnection());
+      await store.saveSessionKey(PEER_SESSION_KEY, peerId, peerPublicKeyData.creationDate);
+
+      const key = await store.retrieveLastSessionKey(peerId);
+
+      expect(key!.keyId.equals(peerPublicKeyData.keyId)).toBeTrue();
+      expect(await derSerializePublicKey(key!.publicKey)).toEqual(peerPublicKeyData.keyDer);
+    });
+
+    test('Non-existing key should result in null', async () => {
+      const store = new MongoPublicKeyStore(getConnection());
+
+      await expect(store.retrieveLastSessionKey(peerId)).resolves.toBeNull();
+    });
   });
 });
--- src/lib/MongoPublicKeyStore.ts
+++ src/lib/MongoPublicKeyStore.ts
@@ -2,29 +2,39 @@ import { PublicKeyStore, SessionPublicKeyData } from '@relaycorp/relaynet-core';
 import { getModelForClass } from '@typegoose/typegoose';
 import { Connection, Model } from 'mongoose';
 
-import { PeerPublicKeyData } from './models';
+import { IdentityPublicKeyData, PeerPublicKeyData } from './models';
 
 export class MongoPublicKeyStore extends PublicKeyStore {
-  protected readonly keyDataModel: Model<any>;
+  protected readonly identityKeyDataModel: Model<any>;
+  protected readonly sessionKeyDataModel: Model<any>;
 
   constructor(connection: Connection) {
     super();
 
-    this.keyDataModel = getModelForClass(PeerPublicKeyData, {
+    this.identityKeyDataModel = getModelForClass(IdentityPublicKeyData, {
+      existingConnection: connection,
+    });
+    this.sessionKeyDataModel = getModelForClass(PeerPublicKeyData, {
       existingConnection: connection,
     });
   }
 
-  protected retrieveIdentityKeySerialized(_peerId: string): Promise<Buffer | null> {
-    throw new Error('Method not yet implemented');
+  protected async retrieveIdentityKeySerialized(peerId: string): Promise<Buffer | null> {
+    const query = this.identityKeyDataModel.findOne({ peerId });
+    const keyData: null | IdentityPublicKeyData = await query.exec();
+    return keyData?.keyDer ?? null;
   }
 
-  protected saveIdentityKeySerialized(_keySerialized: Buffer, _peerId: string): Promise<void> {
-    throw new Error('Method not yet implemented');
+  protected async saveIdentityKeySerialized(keySerialized: Buffer, peerId: string): Promise<void> {
+    const dbData: IdentityPublicKeyData = {
+      keyDer: keySerialized,
+      peerId,
+    };
+    await this.identityKeyDataModel.updateOne({ peerId }, dbData, { upsert: true }).exec();
   }
 
   protected async retrieveSessionKeyData(peerId: string): Promise<SessionPublicKeyData | null> {
-    const query = this.keyDataModel.findOne({ peerId });
+    const query = this.sessionKeyDataModel.findOne({ peerId });
     const keyData: null | PeerPublicKeyData = await query.exec();
     if (keyData === null) {
       return null;
@@ -43,6 +53,6 @@ export class MongoPublicKeyStore extends PublicKeyStore {
       keyId: keyData.publicKeyId,
       peerId,
     };
-    await this.keyDataModel.updateOne({ peerId }, dbData, { upsert: true }).exec();
+    await this.sessionKeyDataModel.updateOne({ peerId }, dbData, { upsert: true }).exec();
   }
 }
