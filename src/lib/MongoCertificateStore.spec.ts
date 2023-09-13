845d7063167b9ae57eab69bb00fa76816acd0b2a
Gus Narea <gnarea@users.noreply.github.com>
2023-09-13 17:04:08 +0100

    fix(CertificateStore): Fix filter in upsert
    As it was previously overriding records simply if the expiry date matched.

--- src/lib/MongoCertificateStore.spec.ts
+++ src/lib/MongoCertificateStore.spec.ts
@@ -21,26 +21,40 @@ beforeAll(async () => {
   store = new MongoCertificateStore(connection);
 });
 
-let identityKeyPair: CryptoKeyPair;
+let issuerPrivateKey: CryptoKey;
+let issuerId: string;
+let issuerCertificate: Certificate;
+let subjectKeyPair: CryptoKeyPair;
 let subjectId: string;
 let validCertificate: Certificate;
 let validCertificationPath: CertificationPath;
 let expiredCertificate: Certificate;
 let expiredCertificationPath: CertificationPath;
 beforeAll(async () => {
-  identityKeyPair = await generateRSAKeyPair();
-  subjectId = await getIdFromIdentityKey(identityKeyPair.publicKey);
+  const issuerKeyPair = await generateRSAKeyPair();
+  issuerPrivateKey = issuerKeyPair.privateKey;
+  issuerCertificate = await issueGatewayCertificate({
+    issuerPrivateKey: issuerKeyPair.privateKey,
+    subjectPublicKey: issuerKeyPair.publicKey,
+    validityEndDate: addDays(new Date(), 1),
+  });
+  issuerId = await issuerCertificate.calculateSubjectId();
+
+  subjectKeyPair = await generateRSAKeyPair();
+  subjectId = await getIdFromIdentityKey(subjectKeyPair.publicKey);
 
   validCertificate = await issueGatewayCertificate({
-    issuerPrivateKey: identityKeyPair.privateKey,
-    subjectPublicKey: identityKeyPair.publicKey,
-    validityEndDate: addSeconds(new Date(), 15),
+    subjectPublicKey: subjectKeyPair.publicKey,
+    issuerPrivateKey: subjectKeyPair.privateKey,
+    issuerCertificate,
+    validityEndDate: issuerCertificate.expiryDate,
   });
   validCertificationPath = new CertificationPath(validCertificate, []);
 
   expiredCertificate = await issueGatewayCertificate({
-    issuerPrivateKey: identityKeyPair.privateKey,
-    subjectPublicKey: identityKeyPair.publicKey,
+    subjectPublicKey: subjectKeyPair.publicKey,
+    issuerCertificate,
+    issuerPrivateKey,
     validityEndDate: subSeconds(new Date(), 1),
     validityStartDate: subSeconds(new Date(), 2),
   });
@@ -49,75 +63,102 @@ beforeAll(async () => {
 
 describe('saveData', () => {
   test('All attributes should be saved', async () => {
-    await store.save(validCertificationPath, subjectId);
+    await store.save(validCertificationPath, issuerId);
 
-    const certificateStored = await certificateModel.findOne({ subjectId }).exec();
-    expect(certificateStored?.expiryDate).toEqual(validCertificate.expiryDate);
-    expect(certificateStored?.issuerId).toEqual(subjectId);
-    expect(Buffer.from(certificateStored!.pathSerialized)).toEqual(
-      Buffer.from(validCertificationPath.serialize()),
-    );
+    const certificateRecords = await certificateModel.find({ subjectId }).exec();
+    expect(certificateRecords).toHaveLength(1);
+    expect(certificateRecords).toContainEqual(await makeCertPathMatch(validCertificationPath));
   });
 
-  test('The same subject should be allowed to have multiple certificates', async () => {
+  test('The same subject should be allowed to have multiple paths from different issuers', async () => {
+    const issuer2KeyPair = await generateRSAKeyPair();
+    const issuer2Certificate = await issueGatewayCertificate({
+      issuerPrivateKey: issuer2KeyPair.privateKey,
+      subjectPublicKey: issuer2KeyPair.publicKey,
+      validityEndDate: addDays(validCertificate.expiryDate, 1),
+    });
     const certificate2 = await issueGatewayCertificate({
-      issuerPrivateKey: identityKeyPair.privateKey,
-      subjectPublicKey: identityKeyPair.publicKey,
+      subjectPublicKey: subjectKeyPair.publicKey,
+      issuerPrivateKey: issuer2KeyPair.privateKey,
+      issuerCertificate: issuer2Certificate,
       validityEndDate: addDays(validCertificate.expiryDate, 1),
     });
     const certificationPath2 = new CertificationPath(certificate2, []);
 
-    await store.save(validCertificationPath, subjectId);
-    await store.save(certificationPath2, subjectId);
+    await store.save(validCertificationPath, issuerId);
+    await store.save(certificationPath2, issuer2Certificate.getIssuerId()!);
 
     const certificateRecords = await certificateModel.find({ subjectId }).exec();
     expect(certificateRecords).toHaveLength(2);
-    expect(certificateRecords[0]?.expiryDate).toEqual(validCertificate.expiryDate);
-    expect(Buffer.from(certificateRecords[0]!.pathSerialized)).toEqual(
-      Buffer.from(validCertificationPath.serialize()),
-    );
-    expect(certificateRecords[1]?.expiryDate).toEqual(certificate2.expiryDate);
-    expect(Buffer.from(certificateRecords[1]!.pathSerialized)).toEqual(
-      Buffer.from(certificationPath2.serialize()),
-    );
+    expect(certificateRecords).toContainEqual(await makeCertPathMatch(validCertificationPath));
+    expect(certificateRecords).toContainEqual(await makeCertPathMatch(certificationPath2));
   });
 
-  test('Certificates with the same subject and expiry should be deduped within same scope', async () => {
+  test('The same issuer should be allowed to have multiple certificates for different subjects', async () => {
+    const subject2KeyPair = await generateRSAKeyPair();
+    const subject2Certificate = await issueGatewayCertificate({
+      subjectPublicKey: subject2KeyPair.publicKey,
+      issuerPrivateKey,
+      issuerCertificate,
+      validityEndDate: validCertificate.expiryDate,
+    });
+    const certificationPath2 = new CertificationPath(subject2Certificate, []);
+
+    await store.save(validCertificationPath, issuerId);
+    await store.save(certificationPath2, issuerId);
+
+    const certificateRecords = await certificateModel.find({ issuerId }).exec();
+    expect(certificateRecords).toHaveLength(2);
+    expect(certificateRecords).toContainEqual(await makeCertPathMatch(validCertificationPath));
+    expect(certificateRecords).toContainEqual(await makeCertPathMatch(certificationPath2));
+  });
+
+  test('Multiple paths with the same subject and issuer should be allowed if expiry dates differ', async () => {
     const certificate2 = await issueGatewayCertificate({
-      issuerPrivateKey: identityKeyPair.privateKey,
-      subjectPublicKey: identityKeyPair.publicKey,
+      subjectPublicKey: subjectKeyPair.publicKey,
+      issuerPrivateKey,
+      issuerCertificate,
+      validityEndDate: subSeconds(validCertificate.expiryDate, 1),
+    });
+    const certificationPath2 = new CertificationPath(certificate2, []);
+
+    await store.save(validCertificationPath, issuerId);
+    await store.save(certificationPath2, issuerId);
+
+    const certificateRecords = await certificateModel.find({ subjectId }).exec();
+    expect(certificateRecords).toHaveLength(2);
+    expect(certificateRecords).toContainEqual(await makeCertPathMatch(validCertificationPath));
+    expect(certificateRecords).toContainEqual(await makeCertPathMatch(certificationPath2));
+  });
+
+  test('Multiple paths with the same subject and issuer should be deduped if expiry dates match', async () => {
+    const certificate2 = await issueGatewayCertificate({
+      subjectPublicKey: subjectKeyPair.publicKey,
+      issuerPrivateKey,
+      issuerCertificate,
       validityEndDate: validCertificate.expiryDate,
     });
     const certificationPath2 = new CertificationPath(certificate2, []);
 
-    await store.save(validCertificationPath, subjectId);
-    await store.save(certificationPath2, subjectId);
+    await store.save(validCertificationPath, issuerId);
+    await store.save(certificationPath2, issuerId);
 
     const certificateRecords = await certificateModel.find({ subjectId }).exec();
     expect(certificateRecords).toHaveLength(1);
-    expect(certificateRecords[0]?.expiryDate).toEqual(certificate2.expiryDate);
-    expect(Buffer.from(certificateRecords[0]!.pathSerialized)).toEqual(
-      Buffer.from(certificationPath2.serialize()),
-    );
+    expect(certificateRecords).toContainEqual(await makeCertPathMatch(certificationPath2));
   });
 
-  test('Certificates with the same subject and expiry should not be deduped across scopes', async () => {
-    const certificate2 = await issueGatewayCertificate({
-      issuerPrivateKey: identityKeyPair.privateKey,
-      subjectPublicKey: identityKeyPair.publicKey,
-      validityEndDate: validCertificate.expiryDate,
+  async function makeCertPathMatch(certificationPath: CertificationPath): Promise<jest.Expect> {
+    const leafCertificate = certificationPath.leafCertificate;
+    return expect.objectContaining({
+      expiryDate: leafCertificate.expiryDate,
+      issuerId: leafCertificate.getIssuerId()!,
+      subjectId: await leafCertificate.calculateSubjectId(),
+      pathSerialized: expect.toSatisfy((serialisation) =>
+        Buffer.from(certificationPath.serialize()).equals(Buffer.from(serialisation)),
+      ),
     });
-
-    await store.save(validCertificationPath, subjectId);
-    await store.save(new CertificationPath(certificate2, []), subjectId);
-
-    await expect(
-      certificateModel.find({ subjectId, issuerId: subjectId }).exec(),
-    ).resolves.toHaveLength(1);
-    await expect(
-      certificateModel.find({ subjectId, issuerId: subjectId }).exec(),
-    ).resolves.toHaveLength(1);
-  });
+  }
 });
 
 describe('retrieveLatestSerialization', () => {
@@ -134,8 +175,8 @@ describe('retrieveLatestSerialization', () => {
   test('The latest valid certificate should be returned', async () => {
     await store.save(validCertificationPath, subjectId);
     const newestCertificate = await issueGatewayCertificate({
-      issuerPrivateKey: identityKeyPair.privateKey,
-      subjectPublicKey: identityKeyPair.publicKey,
+      issuerPrivateKey: subjectKeyPair.privateKey,
+      subjectPublicKey: subjectKeyPair.publicKey,
       validityEndDate: addSeconds(validCertificate.expiryDate, 1),
     });
     await store.save(new CertificationPath(newestCertificate, []), subjectId);
@@ -146,10 +187,8 @@ describe('retrieveLatestSerialization', () => {
   });
 
   test('Certificate from different issuer should be ignored', async () => {
-    await store.save(validCertificationPath, subjectId);
-    const issuerId = `not-${subjectId}`;
-
-    await expect(store.retrieveLatest(subjectId, issuerId)).resolves.toBeNull();
+    await store.save(validCertificationPath, issuerId);
+    await expect(store.retrieveLatest(subjectId, `not-${issuerId}`)).resolves.toBeNull();
   });
 });
 
@@ -165,39 +204,32 @@ describe('retrieveAllSerializations', () => {
   });
 
   test('All valid certificates should be returned', async () => {
-    await store.save(expiredCertificationPath, subjectId);
-    await store.save(validCertificationPath, subjectId);
-    const newestCertificate = await issueGatewayCertificate({
-      issuerPrivateKey: identityKeyPair.privateKey,
-      subjectPublicKey: identityKeyPair.publicKey,
-      validityEndDate: addSeconds(validCertificate.expiryDate, 1),
+    await store.save(validCertificationPath, issuerId);
+    const certificate2 = await issueGatewayCertificate({
+      subjectPublicKey: subjectKeyPair.publicKey,
+      issuerPrivateKey,
+      issuerCertificate,
+      validityEndDate: subSeconds(validCertificate.expiryDate, 1),
     });
-    await store.save(new CertificationPath(newestCertificate, []), subjectId);
+    await store.save(new CertificationPath(certificate2, []), issuerId);
 
-    const allCertificates = await store.retrieveAll(subjectId, subjectId);
+    const allPaths = await store.retrieveAll(subjectId, issuerId);
 
-    expect(allCertificates).toHaveLength(2);
-    expect(
-      allCertificates.filter((p) => p.leafCertificate.isEqual(validCertificate)),
-    ).not.toBeEmpty();
-    expect(
-      allCertificates.filter((p) => p.leafCertificate.isEqual(newestCertificate)),
-    ).not.toBeEmpty();
-  });
-
-  test('Scope should be temporarily stored', async () => {
-    await store.save(validCertificationPath, subjectId);
-
-    const allCertificates = await store.retrieveAll(subjectId, subjectId);
-
-    expect(allCertificates).toHaveLength(1);
-    expect(allCertificates[0].leafCertificate.isEqual(validCertificate)).toBeTrue();
+    expect(allPaths).toHaveLength(2);
+    expect(allPaths).toContainEqual(
+      expect.objectContaining({
+        leafCertificate: expect.toSatisfy((c) => c.isEqual(validCertificate)),
+      }),
+    );
+    expect(allPaths).toContainEqual(
+      expect.objectContaining({
+        leafCertificate: expect.toSatisfy((c) => c.isEqual(certificate2)),
+      }),
+    );
   });
 
   test('Certificate from different issuer should be ignored', async () => {
-    await store.save(validCertificationPath, subjectId);
-    const issuerId = `not-${subjectId}`;
-
+    await store.save(validCertificationPath, `not-${issuerId}`);
     await expect(store.retrieveAll(subjectId, issuerId)).resolves.toBeEmpty();
   });
 });
